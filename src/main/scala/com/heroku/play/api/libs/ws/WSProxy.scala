ed8e76937b2496bc747ffcc20114d734a4fa16b8
Scott Clasen <scott@heroku.com>
2013-02-25 15:58:43 -0800

    proxy stream chunked

--- src/main/scala/com/heroku/play/api/libs/ws/WSProxy.scala
+++ src/main/scala/com/heroku/play/api/libs/ws/WSProxy.scala
@@ -1,14 +1,18 @@
 package com.heroku.play.api.libs.ws
 
-import play.api.libs.iteratee.{ Concurrent, Enumerator }
+import play.api.libs.iteratee.{ Iteratee, Concurrent, Enumerator }
 import com.ning.http.client._
 import com.ning.http.client.AsyncHandler.STATE
-import play.api.mvc.{ Results, Result, ResponseHeader, SimpleResult }
+import play.api.mvc._
 import play.api.libs.ws.WS
 import concurrent.{ Future, Promise, future, promise }
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.mvc.ResponseHeader
+import com.ning.http.client.Request
+import play.api.mvc.SimpleResult
+import play.api.http.Status
 
-object WSProxy {
+object WSProxy extends Controller {
 
   def proxyGetAsync(url: String, responseHeadersToOverwrite: (String, String)*) = proxyRequestAsync(WS.client.prepareGet(url).build(), responseHeadersToOverwrite.toMap)
 
@@ -57,7 +61,9 @@ object WSProxy {
             val hmap = h.getHeaders.iterator().asScala.map {
               entry => entry.getKey -> entry.getValue.get(0)
             }.toMap ++ responseHeadersToOverwrite
-            if (h.getHeaders.containsKey("Content-Length") && h.getHeaders.get("Content-Length").get(0) != "0") {
+            if (h.getHeaders.containsKey("transfer-encoding") && h.getHeaders.get("transfer-encoding").get(0) == "chunked") {
+              Status(s).stream(enum).withHeaders(hmap.toSeq: _*)
+            } else if (h.getHeaders.containsKey("Content-Length") && h.getHeaders.get("Content-Length").get(0) != "0") {
               SimpleResult(ResponseHeader(s, hmap), enum)
             } else {
               SimpleResult(ResponseHeader(s, hmap), Enumerator(Results.EmptyContent()))
